1ce4bcd6f491f5d59e3f942ad556687afcbda270
petcu1004 <kty4585@naver.com>
2023-01-09 17:18:00 +0900

    Comment 데이터를 별도의 객체로 분리하기 => 댓글 컴포넌트 만들기 완성

--- my-app/src/chapter_05/CommentList.jsx
+++ my-app/src/chapter_05/CommentList.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments= [
+    {
+        name:"민새미",
+        comment:"안녕하세요",
+    },
+    {
+        name:"유재석",
+        comment:"리액트 재미있어요!",
+    },
+    {
+        name:" 강민경",
+        comment:"저도 배울래요",
+    },
+];
+
 function CommentList(props){
     return(
         <div>
-            <Comment name={"민새미"} comment={"안녕하세요, 민새미입니다."}/>
-            <Comment name={"유재석"} comment={"리액트 재미있어요~"}/>
+            {comments.map((comment) => {
+                return(
+                    <Comment name={comment.name} comment={comment.comment}/>
+                    );
+            })}
         </div>
     );
 }
