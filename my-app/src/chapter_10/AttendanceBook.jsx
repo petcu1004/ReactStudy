28a795b15aad9bd342cd7afc2c754e49e9d53097
petcu1004 <kty4585@naver.com>
2023-01-17 22:02:11 +0900

    id, key 추가

--- my-app/src/chapter_10/AttendanceBook.jsx
+++ my-app/src/chapter_10/AttendanceBook.jsx
@@ -2,15 +2,19 @@ import React from "react";
 
 const students=[
     {
+        id: 1,
         name:"Inje",
     },
     {
+        id:2,
         name:"Steve",
     },
     {
+        id:3,
         name:"Bill",
     },
     {
+        id:4,
         name:"Jeff",
     },
 ];
@@ -19,7 +23,7 @@ function AttendanceBook(props){
     return(
         <ul>
             {students.map((student)=>{
-                return <li>{student.name}</li>
+                return <li key={student.id}>{student.name}</li>
             })}
         </ul>
     );
