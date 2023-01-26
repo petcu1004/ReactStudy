e5dee46d79805d74c69dd463128ba4bfe9e091c7
petcu1004 <kty4585@naver.com>
2023-01-26 17:39:28 +0900

    Mini Project - 미니 블로그 만들기

--- mini-blog/src/component/page/MainPage.jsx
+++ mini-blog/src/component/page/MainPage.jsx
@@ -3,11 +3,11 @@ import {useNavigate} from "react-router-dom";
 import styled from "styled-components";
 import PostList from "../list/PostList";
 import Button from "../ui/Button";
-import data from '../../data.json';
+import data from "../../data.json"
 
 const Wrapper = styled.div`
     padding: 16px;
-    width: calc(100%-32px);
+    width: calc(100% - 32px);
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -18,7 +18,7 @@ const Container=styled.div`
     width: 100%;
     max-width: 720px;
 
-    &>*{
+    & > * {
         :not(:last-child){
             margin-bottom: 16px;
         }
@@ -38,6 +38,13 @@ function MainPage(props){
                 onClick={() => {
                     navigate("/post-write");
                 }} 
+                />
+
+                <PostList
+                    posts={data}
+                    onClickItem={(item) => {
+                        navigate(`/post/${item.id}`);
+                    }}
                 />
             </Container>
         </Wrapper>
--- mini-blog/src/component/ui/TextInput.jsx
+++ mini-blog/src/component/ui/TextInput.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledTextarea=styled.textarea`
+const StyledTextarea = styled.textarea`
     width: calc(100% - 32px);
     ${(props) =>
-    props.heigth &&`
-    heigth : ${props.heigth}px;
+        props.height &&
+        `
+        height: ${props.height}px;
     `}
     padding: 16px;
     font-size: 16px;
     line-height: 20px;
 `;
 
-function TextInput(props){
-    const { heigth, value, onChange} = props;
+function TextInput(props) {
+    const { height, value, onChange } = props;
     // 높이 설정을 위한 heigth
     // 입력된 값을 표시하기 위한 value
     // 변경된 값을 상위 컴포넌트로 전달하기 위한 onChange
-
-    return <StyledTextarea heigth ={heigth} value={value} onChange={onChange} />;
+    return <StyledTextarea height={height} value={value} onChange={onChange} />;
 }
 
 export default TextInput;
